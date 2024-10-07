c859be63ff7f9790a531ac9aabac8605f55c86f7
AnirbanChattopadhyay-2004 <anirban.chtt@gmail.com>
2024-10-07 10:28:56 +0530

    Added types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function TextToImageConverter() {
   const [prompt, setPrompt] = useState('')
   const [imageUrl, setImageUrl] = useState('')
   const [isLoading, setIsLoading] = useState(false)
-  async function query(data) {
+  async function query(data:{inputs:string}) {
     const response = await fetch(
         "https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
         {
